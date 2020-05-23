9e5358627f40223e9a6c29c181a96903d0a1aee8
Abby P Joby <abbypjoby@gmail.com>
2020-05-23 21:31:50 +0530

    all features first cut

--- home_controller/main/gaia_main.c
+++ home_controller/main/gaia_main.c
@@ -7,23 +7,25 @@
 #include "esp_system.h" 
 #include "esp_log.h"
 #include "wifi_manager.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include "cJSON.h"
+
+#include "server_connection.c"
 #include "io_pins.c"
 
+#define OPEN_STATUS 1
+#define CLOSE_STATUS 0
+#define STOP_STATUS -1
+#define DEFAULT_STATUS 2
 
-bool isControlManual = true; 
+
+bool isControlManual = false; 
+int gateStatus = DEFAULT_STATUS;
+int buttonPressStatus = DEFAULT_STATUS;
 static xQueueHandle gpio_evt_queue = NULL;
 
-void wifi_on_connected(void *pvParameter){
-    isControlManual = false; 
-	ESP_LOGI("main", "--------- WiFi Connected");
-}
-
-void wifi_on_disconnected(void *pvParameter){
-    isControlManual = true; 
-	ESP_LOGI("main", "--------- WiFi Disconnected");
-    // Try to reconnect
-}
-
+TaskHandle_t refresh_gate_status_handler = NULL;
 
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
@@ -43,15 +45,15 @@ static void handle_switch_press(void* arg)
             switch (switch_num)
             {
                 case OPEN_SWITCH_INPUT:
-                    openGate();
+                    buttonPressStatus = OPEN_STATUS;
                     break;
 
                 case CLOSE_SWITCH_INPUT:
-                    closeGate();
+                    buttonPressStatus = CLOSE_STATUS;
                     break;
 
                 case STOP_SWITCH_INPUT:
-                    stopGate();
+                    buttonPressStatus = STOP_STATUS;
                     break;
                 
                 default:
@@ -65,6 +67,52 @@ static void handle_switch_press(void* arg)
 }
 
 
+
+// Keep updating gateStatus every 2 seconds
+void refresh_gate_status(void *pvParameters)
+{
+    for(;;)
+    {
+        gateStatus = fetch_gate_status();
+        ESP_LOGI(TAG, "Finish http example");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+// If in manual Mode, update the Server with buttonPressStatus
+void update_server_gate_status(void *pvParameters)
+{
+    for(;;)
+    {
+        if (isControlManual && buttonPressStatus >= -1 && buttonPressStatus <= 1)
+        {   
+            // Update server here
+            update_gate_status(buttonPressStatus);
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+            // if server started to respond, turn on Auto Control Mode
+            if (gateStatus == buttonPressStatus){
+                isControlManual = false;
+            }
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+
+void wifi_on_connected(void *pvParameter){
+    isControlManual = false; 
+    //API call - start task and refresh gateStatus every 2 seconds
+	ESP_LOGI("main", "--------- WiFi Connected");
+}
+
+void wifi_on_disconnected(void *pvParameter){
+    isControlManual = true; 
+	ESP_LOGI("main", "--------- WiFi Disconnected");
+
+    // Try to reconnect
+}
 
 void app_main()
 {
@@ -80,27 +128,64 @@ void app_main()
     gpio_evt_queue = xQueueCreate(1, sizeof(uint32_t));
     xTaskCreate(handle_switch_press, "handle_switch_press", 2048, NULL, 10, NULL);
 
-
+    // Start two tasks, one for refreshing gate status, another to update with manual override values
+    xTaskCreate(refresh_gate_status, "refresh_gate_status", 8192, NULL, 5, NULL);
+    xTaskCreate(update_server_gate_status, "update_server_gate_status", 8192, NULL, 5, NULL);
 
     for(;;)
     {
         if (isControlManual)
         {
-            // Read Manual button signals / use saved state
-
-            // Update output
-            
+            ESP_LOGI("main","Manual Mode Active");
             // Try to update the Server with Manual Signal
             // if successful, turn to serverControlMode
+            switch (buttonPressStatus)
+            {
+                case CLOSE_STATUS:
+                    closeGate();
+                    break;
 
+                case OPEN_STATUS:
+                    openGate();
+                    break;
+
+                case STOP_STATUS:
+                    stopGate();
+                    break;
+            
+                default:
+                    ESP_LOGE("main", "Unknown Button Press Status in Manual Control Mode");
+                    stopGate();
+                    break;
+            }
         } 
         
         else
         {   
-            // Call API to fetch latest state
-            // Update Output
+            ESP_LOGI("main","Auto Mode Active");
+            switch (gateStatus)
+            {
+                case CLOSE_STATUS:
+                    closeGate();
+                    break;
+
+                case OPEN_STATUS:
+                    openGate();
+                    break;
+
+                case STOP_STATUS:
+                    stopGate();
+                    break;
+                    
+                default:
+                    // status 2 means API call failed, so some network issue,
+                    // therefore set to manualControl mode
+                    stopGate();
+                    isControlManual = true;
+                    break;
+            }
         }
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 
 }
---
+++ home_controller/main/server_connection.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "esp_system.h" 
+#include "esp_log.h"
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include "cJSON.h"
+
+
+static const char *TAG = "Server_Connection";
+esp_err_t _http_event_handle(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+            printf("%.*s", evt->data_len, (char*)evt->data);
+            break;
+
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (esp_http_client_is_chunked_response(evt->client)) {
+                printf("%.*s", evt->data_len, (char*)evt->data);
+                if (evt->user_data) {
+                    memcpy(evt->user_data, evt->data, evt->data_len);
+                }
+            }
+            break;
+
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+    }
+    return ESP_OK;
+}
+
+
+
+int fetch_gate_status(void)
+{
+    int is_on = 2;
+    char local_response_buffer[1024] = {0};
+
+    esp_http_client_config_t config = {
+        .url = "http://pallathatta.herokuapp.com/node/1",
+        .event_handler = _http_event_handle,
+        .user_data = local_response_buffer
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+    ESP_LOGI(TAG, "Status = %d, content_length = %d",
+            esp_http_client_get_status_code(client),
+            esp_http_client_get_content_length(client));
+
+        cJSON *root = cJSON_Parse(local_response_buffer);
+        is_on = cJSON_GetObjectItem(root,"is_on")->valueint;
+        ESP_LOGI(TAG, "is_on = %d", is_on);
+        cJSON_Delete(root);
+    }
+    esp_http_client_cleanup(client);
+
+    return is_on;
+}
+
+
+int update_gate_status(int status)
+{
+    int is_on = 2;
+    char local_response_buffer[1024] = {0};
+
+    char uri[100];
+    snprintf(uri, sizeof(uri), "http://pallathatta.herokuapp.com/node/update?node_id=1&is_on=%d",status);
+
+    ESP_LOGI(TAG, "URI to update %s", uri);
+    esp_http_client_config_t config = {
+        .url = uri,
+        .event_handler = _http_event_handle,
+        .user_data = local_response_buffer
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+    ESP_LOGI(TAG, "Successfully Update Gate Status, ResponseStatus = %d, content_length = %d",
+            esp_http_client_get_status_code(client),
+            esp_http_client_get_content_length(client));
+    }
+    esp_http_client_cleanup(client);
+
+    return is_on;
+}
+
